a6c87aedf18cc749193502d3d8a31e80d332fb14
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-26 10:26:49 +0530

    Add files via upload

---
+++ variable_practice.sh
@@ -0,0 +1,16 @@
+
+student="Sarah"
+echo "Hello ${student}"
+echo "Hello "+ ${student}
+
+echo "============"
+
+employee="John"
+
+echo "Dear ${employee}"
+echo "The board I would like to thank you for the execellent work that you've done"
+echo "Keep the good work ${employee}"
+
+echo "Best Wishes,"
+echo "Prateel, CEO"
+
