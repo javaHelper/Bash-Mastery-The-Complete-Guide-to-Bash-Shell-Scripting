0143fd0c7acd2ad5ff7f3266a68787f96524fcb3
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-26 15:42:29 +0530

    Update conditional_operators.sh

--- conditional_operators.sh
+++ conditional_operators.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-echo "[[ 2 -eq 2 ]]"
-[[ 2 -eq 2 ]]; echo $?
-
-echo "-----------------------"
-
-echo "[[ 1 -eq 2 ]]"
-[[ 1 -eq 2 ]]; echo $?
-echo "--------------"
-
 echo "[[ 1 -ne 2 ]]"
 [[ 1 -ne 2 ]]; echo $?
 echo "---------------"
@@ -19,4 +8,21 @@ echo "----------------"
 
 aa=hello
 b=goodbye
+
+echo "[[ $a = $b ]]"
 [[ $a = $b ]] ; echo $?
+          
+echo "[[ $a != $b ]]"
+[[ $a != $b ]] ; echo $?
+
+echo "[[ -z $c ]]"
+[[ -z $c ]] ; echo $? 
+
+c=anything
+[[ -z $c ]] ; echo $? 
+    
+echo "[[ -e today.txt ]]"
+[[ -e today.txt ]] ; echo $? 
+    
+touch today.txt
+[[ -e today.txt ]] ; echo $?
