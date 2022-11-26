d5ccee2fa8883bea1163c7f812c67239f2ee9b15
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-26 11:30:20 +0530

    Create brace_expansion.sh

---
+++ brace_expansion.sh
@@ -0,0 +1,20 @@
+echo {a,19,z,barry,42}
+
+echo {jan,feb,mar,apr,may,jun}
+
+echo {1..10}
+
+echo {10..1}
+
+echo {a..z}
+
+echo {1..100..3}
+
+echo x{1..10}
+
+echo month{0..12}
+
+#This will create month01 .... month12 directory
+#mkdir month{01..12}
+
+#echo month{01..12}/day{01..31}.txt
