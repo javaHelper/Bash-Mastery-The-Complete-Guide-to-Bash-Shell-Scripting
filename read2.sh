3650460d04cf8fe6a67887233fa5ff547fc11b2d
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-26 13:56:38 +0530

    Update read2.sh

--- read2.sh
+++ read2.sh
@@ -2,3 +2,7 @@
 read -p "Input your first name : " name
 read -p "Input your age : " age
 read -p "Input the town you are from: " town
+
+echo "My name is $name"
+echo "I am $age"
+echo "I am from $town"
