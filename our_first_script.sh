3550fa62ceb5b789be4595879360f94a48485a2d
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-26 09:31:08 +0530

    Add files via upload

---
+++ our_first_script.sh
@@ -0,0 +1,14 @@
+
+# Author: Prateek
+# Date created: 17-Nov-22
+# Last Modified: 17-Nov-22
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# our_first_script
+
+echo "This is my first script"
+exit 0
+
